a4e140efa68cc56550fc3b759635966972bdb779
lozunoff <andrey.lozunov@gmail.com>
2020-05-23 13:07:46 +0400

    В приложении настроена маршрутизация

---
+++ routes/courses.js
@@ -0,0 +1,111 @@
+const { Router } = require('express');
+const { validationResult } = require('express-validator');
+
+const Course = require('../models/course');
+
+const auth = require('../middleware/auth');
+
+const { courseValidators } = require('../utils/validators');
+
+const router = new Router();
+
+function isOwner(course, req) {
+  return course.userId.toString() === req.user._id.toString();
+}
+
+router.get('/', async (req, res) => {
+  try {
+    const courses = await Course.find().populate('userId', 'email name');
+
+    res.render('courses', {
+      title: 'Курсы',
+      isCourses: true,
+      userId: req.user ? req.user._id.toString() : null,
+      courses,
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+});
+
+router.get('/:id/edit', auth, async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect('/');
+  }
+
+  try {
+    const course = await Course.findById(req.params.id);
+
+    if (!isOwner(course, req)) {
+      return res.redirect('/courses');
+    }
+
+    res.render('course-edit', {
+      title: `Редактировать ${course.title}`,
+      course,
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+});
+
+router.post('/edit', auth, courseValidators, async (req, res) => {
+  const errors = validationResult(req);
+  const { id } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.status(422).redirect(`/courses/${id}/edit?allow=true`);
+  }
+
+  try {
+    delete req.body.id;
+
+    const course = await Course.findById(id);
+
+    if (!isOwner(course, req)) {
+      return res.redirect('/courses');
+    }
+
+    Object.assign(course, req.body);
+
+    await course.save();
+
+    return res.redirect('/courses');
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+});
+
+router.post('/remove', async (req, res) => {
+  try {
+    await Course.deleteOne({
+      _id: req.body.id,
+      userId: req.user._id,
+    });
+
+    res.redirect('/courses');
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id);
+
+    res.render('course', {
+      layout: 'empty',
+      title: `Курс ${course.title}`,
+      course,
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+});
+
+module.exports = router;
---
+++ routes/home.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+
+const router = new Router();
+
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Главная страница',
+    isHome: true,
+  });
+});
+
+module.exports = router;
